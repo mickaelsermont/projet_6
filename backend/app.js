fa22f71052e370cc251a68976ca0e5783bffb529
Mickaya <31204545+Mickaya@users.noreply.github.com>
2020-12-06 14:23:23 +0100

    fix app.js in backend

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,8 @@ const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://william:william123@cluster0-k7urj.mongodb.net/test?retryWrites=true&w=majority',
-<<<<<<< HEAD
-<<<<<<< HEAD
   { useCreateIndex: true,
     useNewUrlParser: true,
-=======
-  { useNewUrlParser: true,
->>>>>>> 37be648c46e09101cfd9b3eedf55713259113429
-=======
-  { useNewUrlParser: true,
->>>>>>> 37be648c46e09101cfd9b3eedf55713259113429
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
