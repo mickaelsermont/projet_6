2b611e7172d1bdc1293275236d022b90e377abb2
Mickaya <31204545+Mickaya@users.noreply.github.com>
2020-12-08 00:23:11 +0100

    add encryption on email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,24 +1,30 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 require('dotenv/config');
 
 exports.signup = (req, res, next) => {
+    const hash_email = crypto.createHash(process.env.CRYPTO_HASH).update(req.body.email).digest(process.env.CRYPTO_CODING_BASE);
+
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
-                email: req.body.email,
+                email: hash_email,
                 password: hash
             });
+
             user.save()
-            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-            .catch(error => res.status(400).json({ error }));
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    const hash_email = crypto.createHash(process.env.CRYPTO_HASH).update(req.body.email).digest(process.env.CRYPTO_CODING_BASE);
+
+    User.findOne({ email: hash_email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
